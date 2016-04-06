76a7449417ef2ecf34fad246345c640bab741013
Said <said.rehouni@gmail.com>
2016-04-06 22:52:49 +0200

    First commit

---
+++ TalentoMobileAssigment/TalentoMobileAssigment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TalentoMobileAssigment
+//
+//  Created by Said Rehouni on 6/4/16.
+//  Copyright © 2016 Said Rehouni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TalentoMobileAssigment/TalentoMobileAssigment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TalentoMobileAssigment
+//
+//  Created by Said Rehouni on 6/4/16.
+//  Copyright © 2016 Said Rehouni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
